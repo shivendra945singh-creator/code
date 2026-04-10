25d370e2d985028e7e72002d7b3b5c2b235dd375
Shivendra Singh <shivendra945singh@gamil.com>
2026-04-10 14:45:33 +0530

    learning

--- dsa/recursion.java
+++ dsa/recursion.java
@@ -1,17 +1,17 @@
 
-                                           //print numbers from n to 1 in decreasing order
+                                                       //print numbers from n to 1 in decreasing order
                                         
 
 
 public class recursion {
-    public static void printDec(int n) {
-        if (n == 1) {
-            System.out.println(n);
-            return;
-        }
-        System.out.print(n + " ");
-        printDec(n- 1);
-    }
+//     public static void printDec(int n) {
+//         if (n == 1) {
+//             System.out.println(n);
+//             return;
+//         }
+//         System.out.print(n + " ");
+//         printDec(n- 1);
+//     }
 
     // public static void main(String args[]) {
     //     int n = 10;
@@ -54,19 +54,19 @@ public class recursion {
 
 
 
-                                                 //print numbers from 1 to n in increasing order
+                                                          //print numbers from 1 to n in increasing order
 
 
 
 
-    public static void printInc(int n) {
-        if (n == 1) {
-            System.out.print(n + " ");
-            return;
-        }
-        printInc(n - 1);
-        System.out.print(n+" ");
-    }
+    // public static void printInc(int n) {
+    //     if (n == 1) {
+    //         System.out.print(n + " ");
+    //         return;
+    //     }
+    //     printInc(n - 1);
+    //     System.out.print(n+" ");
+    // }
 
     // public static void main(String args[]) {
     //     int n = 10;
@@ -117,7 +117,7 @@ public class recursion {
 
 
 
-                                                  // find factorial of N
+                                                                   // find factorial of N
 
                                                   
 
@@ -125,14 +125,14 @@ public class recursion {
    // time complexity and space complexity: O(n)
 
 
-    public static int factorial(int n) {
-        if (n == 0 || n == 1) {
-            return 1;// base case: factorial of 0 or 1 is 1
-        }
-       int fnm1 = factorial(n - 1); // recursive call to find factorial of (n-1)
-       int fn = n * fnm1; // calculate factorial of n using the result from the recursive call
-       return fn; // return the factorial of n
-    }
+    // public static int factorial(int n) {
+    //     if (n == 0 || n == 1) {
+    //         return 1;// base case: factorial of 0 or 1 is 1
+    //     }
+    //    int fnm1 = factorial(n - 1); // recursive call to find factorial of (n-1)
+    //    int fn = n * fnm1; // calculate factorial of n using the result from the recursive call
+    //    return fn; // return the factorial of n
+    // }
 
 //     public static void main(String args[]) {
 //         int n = 5;
@@ -143,7 +143,7 @@ public class recursion {
 
 
 
-                                            //print sum of first n natural numbers
+                                                           //print sum of first n natural numbers
 
 
 
@@ -151,14 +151,14 @@ public class recursion {
   // time complexity and space complexity: O(n)
 
 
-    public static int  calsum(int n) {
-        if (n == 1) {
-            return 1; // base case: sum of first 1 natural number is 1
-        }
-        int snm1 = calsum(n - 1); // recursive call to find sum of first (n-1) natural numbers
-        int sn = n + snm1; // calculate sum of first n natural numbers using the result from the recursive call
-        return sn; // return the sum of first n natural numbers
-    }
+    // public static int  calsum(int n) {
+    //     if (n == 1) {
+    //         return 1; // base case: sum of first 1 natural number is 1
+    //     }
+    //     int snm1 = calsum(n - 1); // recursive call to find sum of first (n-1) natural numbers
+    //     int sn = n + snm1; // calculate sum of first n natural numbers using the result from the recursive call
+    //     return sn; // return the sum of first n natural numbers
+    // }
 
 //     public static void main(String args[]) {
 //         int n = 5;
@@ -170,7 +170,7 @@ public class recursion {
 
 
 
-                                                    //print Fibonacci series up to n terms
+                                                             //print Fibonacci series up to n terms
 
 
 
@@ -178,21 +178,21 @@ public class recursion {
    // time complexity: O(2^n) and space complexity: O(n)
 
 
-    public static int fib(int n) {
-        if (n == 0) {
-            return 0; // base case: Fibonacci of 0 is 0
-        }
-        if (n == 1) {
-            return 1; // base case: Fibonacci of 1 is 1
-        }
-        int fnm1 = fib(n - 1); // recursive call to find Fibonacci of (n-1)
-        int fnm2 = fib(n - 2); // recursive call to find Fibonacci of (n-2)
-        int fn = fnm1 + fnm2; // calculate Fibonacci of n using the results from the recursive calls
-        return fn; // return the Fibonacci of n
-    }
+    // public static int fib(int n) {
+    //     if (n == 0) {
+    //         return 0; // base case: Fibonacci of 0 is 0
+    //     }
+    //     if (n == 1) {
+    //         return 1; // base case: Fibonacci of 1 is 1
+    //     }
+    //     int fnm1 = fib(n - 1); // recursive call to find Fibonacci of (n-1) term
+    //     int fnm2 = fib(n - 2); // recursive call to find Fibonacci of (n-2) term
+    //     int fn = fnm1 + fnm2; // calculate Fibonacci of n using the results from the recursive calls
+    //     return fn; // return the Fibonacci of n
+    // }
 
 //     public static void main(String args[]) {
-//         int n = 26;
+//         int n = 26; //n is the number of terms in the Fibonacci series to be printed
 //         System.out.println("Fibonacci series up to " + 23 + " terms is:" + fib(n));
 //           System.out.println("Fibonacci series up to " + 24 + " terms is:" + fib(n));
 //              System.out.println("Fibonacci series up to " + 25 + " terms is:" + fib(n));
@@ -204,22 +204,22 @@ public class recursion {
 
 
 
-                                               //check if an array is sorted or not
+                                                          //check if an array is sorted or not
 
 
 
    // time complexity: O(n) and space complexity: O(n)
 
 
-    public static boolean isSorted(int arr[], int i) {
-        if (i == arr.length - 1) {
-            return true; // base case: if we have reached the end of the array, it is sorted
-        }
-        if (arr[i] > arr[i + 1]) {
-            return false; // if the current element is greater than the next element, the array is not sorted
-        }
-        return isSorted(arr, i + 1); // recursive call to check the rest of the array
-    }
+    // public static boolean isSorted(int arr[], int i) {
+    //     if (i == arr.length - 1) {
+    //         return true; // base case: if we have reached the end of the array, it is sorted
+    //     }
+    //     if (arr[i] > arr[i + 1]) {
+    //         return false; // if the current element is greater than the next element, the array is not sorted
+    //     }
+    //     return isSorted(arr, i + 1); // recursive call to check the rest of the array
+    // }
 
     // public static void main(String args[]) {
     //     int arr[] = {1, 2, 3, 4, 5};
@@ -234,7 +234,7 @@ public class recursion {
 
 
 
-                                                       //or
+                                                                      //or
 
 
 
@@ -261,21 +261,21 @@ public class recursion {
 
 
 
-                                             //find the firstoccurrence of an element in an array
+                                                   //find the firstoccurrence of an element in an array
 
 
    // time complexity: O(n) and space complexity: O(n)
 
 
-    public static int firstOcc(int arr[], int key, int i) {
-        if (i == arr.length) {
-            return -1; // base case: i7f we have reached the end of the array without finding the key, return -1
-        }
-        if (arr[i] == key) {
-            return i; // if the current element is equal to the key, return its index
-        }
-        return firstOcc(arr, key, i + 1); // recursive call to check the rest of the array
-    }
+    // public static int firstOcc(int arr[], int key, int i) {
+    //     if (i == arr.length) {
+    //         return -1; // base case: i7f we have reached the end of the array without finding the key, return -1
+    //     }
+    //     if (arr[i] == key) {
+    //         return i; // if the current element is equal to the key, return its index
+    //     }
+    //     return firstOcc(arr, key, i + 1); // recursive call to check the rest of the array
+    // }
 
 //     public static void main(String args[]) {
 //         int arr[] = {1, 2, 3, 4, 3,4,5};
@@ -290,7 +290,7 @@ public class recursion {
 // }                                       
 
 
-                                                       //or
+                                                                           //or
 
 
    // time complexity: O(n) and space complexity: O(n)
@@ -315,7 +315,7 @@ public class recursion {
 
 
 
-                                            // find the last occurrence of an element in an array
+                                                      // find the last occurrence of an element in an array
 
 
 
@@ -350,7 +350,7 @@ public class recursion {
 // }   
 
 
-                                                        //or
+                                                                          //or
 
 
 
@@ -378,7 +378,7 @@ public class recursion {
 // }
 
 
-                                                                 //or
+                                                                       //or
 
 
 
@@ -406,7 +406,7 @@ public class recursion {
 
 
 
-                                                            //find x raised to the power n (x^n)
+                                                               //find x raised to the power n (x^n)
 
 
 
@@ -430,7 +430,7 @@ public class recursion {
 //  }      
 
 
-                                                        //or
+                                                                        //or
 
 
 
@@ -452,17 +452,19 @@ public class recursion {
 //  }  
 
 
-                                                   //or
+                                                                         //or
 
 
    // time complexity: O(n) and space complexity: O(n)
-    public static int power(int x, int n) {
-        if (n == 0) {
-            return 1; // base case: any number raised to the power of 0 is 1
-        }
+
+
+    // public static int power(int x, int n) {
+    //     if (n == 0) {
+    //         return 1; // base case: any number raised to the power of 0 is 1
+    //     }
         
-        return x * power(x, n - 1); // recursive call to find x raised to the power of (n-1) and multiply it by x to get x raised to the power of n
-    }
+    //     return x * power(x, n - 1); // recursive call to find x raised to the power of (n-1) and multiply it by x to get x raised to the power of n
+    // }
 
     // public static void main(String args[]) {
         // int x = 2;
@@ -473,25 +475,25 @@ public class recursion {
 
 
 
-                                             //find x raised to the power n (x^n) optimized
+                                                           //find x raised to the power n (x^n) optimized
 
 
 
     // time complexity: O(log n) and space complexity: O(log n)
-    public static int optimizedPower(int x, int n) {
-        if (n == 0) {
-            return 1; // base case: any number raised to the power of 0 is 1
-        }
-        int halfPower = optimizedPower(x, n / 2); // recursive call to find x raised to the power of (n/2)
-        int halfPowerSq = halfPower * halfPower; // calculate x raised to the power of (n/2) squared
+    // public static int optimizedPower(int x, int n) {
+    //     if (n == 0) {
+    //         return 1; // base case: any number raised to the power of 0 is 1
+    //     }
+    //     int halfPower = optimizedPower(x, n / 2); // recursive call to find x raised to the power of (n/2)
+    //     int halfPowerSq = halfPower * halfPower; // calculate x raised to the power of (n/2) squared
 
-        //n is odd
-        if (n % 2 != 0) { // if n is odd, multiply by x one more time
-            halfPowerSq *= x;
-        }
-        return halfPowerSq; // return x raised to the power of n
+    //     //n is odd
+    //     if (n % 2 != 0) { // if n is odd, multiply by x one more time
+    //         halfPowerSq *= x;
+    //     }
+    //     return halfPowerSq; // return x raised to the power of n
 
-    }
+    // }
 
     // public static void main(String args[]) {
         // int x = 2;
@@ -502,26 +504,26 @@ public class recursion {
 
 
 
-                                               //tiling problem 
+                                                                      //tiling problem 
 
 
 
 
-  public static int tilingProblem(int n) { //2xn board size and 2x1 tiles
-    // base cases: if n is 0 or 1, there is only one way to tile the board
-    if (n == 0 || n == 1) {
-        return 1; // base case: there is one way to tile a 2x0 or 2x1 board
-    }
-    //vertical choice
-    int fnm1 = tilingProblem(n - 1); // recursive call to find the number of ways to tile a 2x(n-1) board
+//   public static int tilingProblem(int n) { //2xn board size and 2x1 tiles
+//     // base cases: if n is 0 or 1, there is only one way to tile the board
+//     if (n == 0 || n == 1) {
+//         return 1; // base case: there is one way to tile a 2x0 or 2x1 board
+//     }
+//     //vertical choice
+//     int fnm1 = tilingProblem(n - 1); // recursive call to find the number of ways to tile a 2x(n-1) board
 
-    //horizontal choice
-    int fnm2 = tilingProblem(n - 2); // recursive call to find the number of ways to tile a 2x(n-2) board
+//     //horizontal choice
+//     int fnm2 = tilingProblem(n - 2); // recursive call to find the number of ways to tile a 2x(n-2) board
 
-    int totalWays = fnm1 + fnm2; // calculate the number of ways to tile a 2xn board using the results from the recursive calls
-    return totalWays; // return the number of ways to tile a 2xn board
+//     int totalWays = fnm1 + fnm2; // calculate the number of ways to tile a 2xn board using the results from the recursive calls
+//     return totalWays; // return the number of ways to tile a 2xn board
       
-  }  
+//   }  
   
   // public static void main(String[] args) {
     // int n = 5;
@@ -532,31 +534,39 @@ public class recursion {
 
 
 
-                                         //remove duplicates in a string
+                                                              //remove duplicates in a string
 
 
- public static void removeDuplicates(String str, int idx, StringBuilder newStr, boolean map[]) {
-    // base case: if we have reached the end of the string, print the new string and return
-    if (idx == str.length()) {
-        System.out.println(newStr);
-        return;
-    }
-    //kaam
-    char currChar = str.charAt(idx); // get the current character
-    if (map[currChar - 'a'] == true) { // if the current character has already been seen, skip it
-      //duplicate
-        removeDuplicates(str, idx + 1, newStr, map);
-    } else { // if the current character has not been seen, add it to the new string and mark it as seen
-        map[currChar - 'a'] = true; // mark the current character as seen
-        removeDuplicates(str, idx + 1, newStr.append(currChar), map); // recursive call to process the rest of the string
-    } 
-  }
+
+
+//  public static void removeDuplicates(String str, int idx, StringBuilder newStr, boolean map[]) {
+//     // base case: if we have reached the end of the string, print the new string and return
+//     if (idx == str.length()) {
+//         System.out.println(newStr);
+//         return;
+//     }
+//     //kaam
+//     char currChar = str.charAt(idx); // get the current character
+//     if (map[currChar - 'a'] == true) { // if the current character has already been seen, skip it
+//       //duplicate
+//         removeDuplicates(str, idx + 1, newStr, map);
+//     } else { // if the current character has not been seen, add it to the new string and mark it as seen
+//         map[currChar - 'a'] = true; // mark the current character as seen
+//         removeDuplicates(str, idx + 1, newStr.append(currChar), map); // recursive call to process the rest of the string
+//     } 
+//   }
   
-    public static void main(String args[]) {
-        String str = "aabbccaapnaaacollegeee";
-        removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
-    }
-  // }
+//     public static void main(String args[]) {
+//         String str = "aabbccaapnaaacollegeee";
+//         removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
+//     }
+//   // }
+
+
+
+
+
+
 
   
-  
+ 
