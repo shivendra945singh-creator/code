66883df54b2e7b7676e432d201fd61f7bc9681ad
Shivendra Singh <shivendra945singh@gamil.com>
2026-04-21 14:01:51 +0530

    LEarn

--- dsa/backtracking.java
+++ dsa/backtracking.java
@@ -307,72 +307,72 @@ public class backtracking {
 
 
 // //time complexity is O(n!)
-public static boolean isSafe(char board[][], int row, int col) {
+// public static boolean isSafe(char board[][], int row, int col) {
 
-//vertical up
-    for(int i=row-1; i>=0; i--) {
-        if(board[i][col] == 'Q') {
-        return false;
-         }
-    }
+// //vertical up
+//     for(int i=row-1; i>=0; i--) {
+//         if(board[i][col] == 'Q') {
+//         return false;
+//          }
+//     }
 
-//diag left up
-    for(int i= row-1, j=col-1; i>=0 && j>=0; i--, j--) {
-        if(board[i][j] == 'Q') {
-            return false;
-        }
-    }
+// //diag left up
+//     for(int i= row-1, j=col-1; i>=0 && j>=0; i--, j--) {
+//         if(board[i][j] == 'Q') {
+//             return false;
+//         }
+//     }
 
-//diag right up
-    for(int i = row-1, j=col+1; i>=0 && j<board.length; i--, j++) {
-         if(board[i][j] == 'Q') {
-        return false;
-        }
-    }
-   return true;
+// //diag right up
+//     for(int i = row-1, j=col+1; i>=0 && j<board.length; i--, j++) {
+//          if(board[i][j] == 'Q') {
+//         return false;
+//         }
+//     }
+//    return true;
 
 
-}
+// }
                                  
-public static void nQueens(char board[][], int row) {
-    //base
-    if(row == board.length) {
-        printBoard(board);
-        return;
-    }
-    //column loop
-    for(int j=0; j<board.length; j++) {
-        if(isSafe(board, row, j)) {
-           board[row][j] ='Q';
-        nQueens(board, row+1);//function call
-        board[row][j] = 'X';//backtracking step 
-        }
+// public static void nQueens(char board[][], int row) {
+//     //base
+//     if(row == board.length) {
+//         printBoard(board);
+//         return;
+//     }
+//     //column loop
+//     for(int j=0; j<board.length; j++) {
+//         if(isSafe(board, row, j)) {
+//            board[row][j] ='Q';
+//         nQueens(board, row+1);//function call
+//         board[row][j] = 'X';//backtracking step 
+//         }
       
-    }
-}
+//     }
+// }
 
-public static void printBoard(char board[][]) {
-    System.out.println("---------chess board--------");
-    for(int i=0; i<board.length; i++) {
-        for(int j=0; j<board.length; j++) {
-            System.out.print(board[i][j] + " ");
-        }
-        System.out.println();
-    }  
-}
+// public static void printBoard(char board[][]) {
+//     System.out.println("---------chess board--------");
+//     for(int i=0; i<board.length; i++) {
+//         for(int j=0; j<board.length; j++) {
+//             System.out.print(board[i][j] + " ");
+//         }
+//         System.out.println();
+//     }  
+// }
 
-public static void main (String args[]) {
-    int n=4;
-    char board[][] = new char[n][n];
-    //initialize
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            board[i][j] ='X';
-        }
-    }
-    nQueens(board,0);
-}
-}
+// public static void main (String args[]) {
+//     int n=4;
+//     char board[][] = new char[n][n];
+//     //initialize
+//     for(int i=0; i<n; i++) {
+//         for(int j=0; j<n; j++) {
+//             board[i][j] ='X';
+//         }
+//     }
+//     nQueens(board,0);
+// }
+// }
 
 
                                               //n Queens with attack(print count ways)
@@ -541,6 +541,7 @@ public static void main (String args[]) {
 
 
                                              //grid ways
+                                             
 
 //(find number of ways to reach from (0,0) to (n-1, m-1) in a nXm grid. allowed moves only right or down)
 // time complexity is O(2^n+m) which is very bad
@@ -566,3 +567,101 @@ public static void main (String args[]) {
     
 // }
 
+
+                                                         //sudoku code
+
+
+
+public static boolean isSafe(int sudoku[][], int row, int col, int digit) {
+   //column
+   for(int i = 0; i<=8; i++) {
+      if(sudoku[i][col] == digit) {
+         return false;
+      }
+   }
+
+   //row
+   for(int j=0; j<=8; j++) {
+      if (sudoku[row][j]  == digit) {
+         return false;
+      }
+   }
+
+   //grid
+
+   int sr = (row/3) * 3;
+   int sc = (col/3) * 3;
+
+   //3*3 GRID
+
+   for(int i=sr; i<sr+3; i++) {
+      for(int j=sc; j<sc+3; j++) {
+         if(sudoku[i][j] == digit) {
+            return false;
+         }
+      }
+   }
+   return true;
+}
+
+public static boolean sudokuSolver (int sudoku[][] , int row , int col) {
+   //base case
+   if ( row == 9 && col == 0) {
+    return true; 
+   }
+
+   //recursion
+   int nextRow = row, nextCol = col +1;
+   if(col+1 == 9) {
+    nextRow = row+1;
+    nextCol = 0;
+   }
+
+   if(sudoku[row][col] !=0 ) {
+    return sudokuSolver(sudoku, nextRow, nextCol);
+   }
+
+   for(int digit = 1; digit<=9; digit++) {
+      if(isSafe(sudoku, row, col, digit)) {
+         sudoku[row][col] = digit;
+         if(sudokuSolver(sudoku, nextRow , nextCol) ) {//solution exists
+                    return true;
+         }
+         sudoku[row][col] = 0;
+      }
+      
+   }
+   return false;
+
+}
+    
+
+public static void printSudoku(int sudoku[][]) {
+   for(int i=0; i<9; i++) {
+      for(int j=0; j<9; j++) {
+         System.out.print(sudoku[i][j]+" ");
+      }
+         System.out.println();
+      }
+   }
+
+   public static void main(String args[]) {
+
+      int sudoku[][] = { {0,0,8,0,0,0,0,0,0},
+      {4,9,0,1,5,7,0,0,2},
+      {0,0,3,0,0,4,1,9,0},
+      {1,8,5,0,6,0,0,2,0},
+      {0,0,0,0,2,0,0,6,0},
+      {9,6,0,4,0,5,3,0,0},
+      {0,3,0,0,7,2,0,0,4},
+      {0,4,9,0,3,0,0,5,7,},
+      {8,2,7,0,0,9,0,1,3}};
+
+      if(sudokuSolver(sudoku,0,0)) {
+         System.out.println("solution exists");
+         printSudoku(sudoku);
+      }else{
+         System.out.println("solution does not exist");
+      }
+   }
+}
