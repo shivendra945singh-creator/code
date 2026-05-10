a58360b76090557a1aa81925a5619b4f7fce5ed1
Shivendra Singh <shivendra945singh@gamil.com>
2026-05-10 19:26:41 +0530

    learn

--- dsa/QueueB.java
+++ dsa/QueueB.java
@@ -442,59 +442,153 @@
                                                         //Queue using two stacks
                                                         
  
-import java.util.*;
-public class QueueB {
- static class Queue {
-    static Stack<Integer> s1 = new Stack<>();
-    static Stack<Integer> s2 = new Stack<>();
+// import java.util.*;
+// public class QueueB {
+//  static class Queue {
+//     static Stack<Integer> s1 = new Stack<>();
+//     static Stack<Integer> s2 = new Stack<>();
 
-    public static boolean isEmpty() {
-        return s1.isEmpty() ;
-    }
+//     public static boolean isEmpty() {
+//         return s1.isEmpty() ;
+//     }
 
-     //add - O(n) time complexity
-    public static void add(int data) {
-        while(!s1.isEmpty()) {
-            s2.push(s1.pop());
-        }
-        s1.push(data);
+//      //add - O(n) time complexity
+//     public static void add(int data) {
+//         while(!s1.isEmpty()) {
+//             s2.push(s1.pop());
+//         }
+//         s1.push(data);
 
-        while(!s2.isEmpty()) {
-            s1.push(s2.pop());
-        }
-    }
+//         while(!s2.isEmpty()) {
+//             s1.push(s2.pop());
+//         }
+//     }
 
-        //remove - O(1) time complexity
-    public static int remove() {
-        if (isEmpty()) {
-            System.out.println("Queue is empty");
-            return -1;
-        }
+//         //remove - O(1) time complexity
+//     public static int remove() {
+//         if (isEmpty()) {
+//             System.out.println("Queue is empty");
+//             return -1;
+//         }
         
-        return s1.pop();
-    }
+//         return s1.pop();
+//     }
 
-    //peek - O(1) time complexity
-    public static int peek() {
-        if (isEmpty()) {
-            System.out.println("Queue is empty");
-            return -1;
+//     //peek - O(1) time complexity
+//     public static int peek() {
+//         if (isEmpty()) {
+//             System.out.println("Queue is empty");
+//             return -1;
+//         }
+//         return s1.peek();
+//     }
+// }
+
+// public static void main(String[] args) {
+//     Queue q = new Queue();
+//     q.add(1);
+//     q.add(2);
+//     q.add(3);
+//     q.add(4);
+//     q.add(5);
+
+//     while (!q.isEmpty()) {
+//         System.out.println(q.peek());
+//         q.remove();
+//     }
+// }
+// }
+
+
+
+                                                //stack using two queues
+
+
+  import java.util.*;
+  import java.util.LinkedList;
+public class QueueB {
+    static class Stack {
+        static Queue<Integer> q1 = new LinkedList<>();
+        static Queue<Integer> q2 = new LinkedList<>();
+
+        public static boolean isEmpty() {
+            return q1.isEmpty() && q2.isEmpty();
         }
-        return s1.peek();
-    }
-}
 
-public static void main(String[] args) {
-    Queue q = new Queue();
-    q.add(1);
-    q.add(2);
-    q.add(3);
-    q.add(4);
-    q.add(5);
+        //push - O(1) time complexity
+        public static void push(int data) {
+           if (!q1.isEmpty()) {
+                q1.add(data);
+            } else {
+               q2.add(data);
+            }
+        }
 
-    while (!q.isEmpty()) {
-        System.out.println(q.peek());
-        q.remove();
+        //pop - O(1) time complexity
+        public static int pop() {
+            if (isEmpty()) {
+                System.out.println("Stack is empty");
+                return -1;
+            }
+           int top = -1;
+           
+           //case 1: when q1 is not empty
+           if (!q1.isEmpty()) {
+               while (!q1.isEmpty()) {
+                   top = q1.remove();
+                   if (q1.isEmpty()) {
+                       break;
+                   }
+                   q2.add(top);
+               }
+           } else { // case 2: when q2 is not empty
+               while (!q2.isEmpty()) {
+                   top = q2.remove();
+                   if (q2.isEmpty()) {
+                          break;
+                   }
+                   q1.add(top);
+               }
+           }
+              return top;
+        }
+
+        //peek - O(1) time complexity
+        public static int peek() {
+            if (isEmpty()) {
+                System.out.println("Stack is empty");
+                return -1;
+            }
+           int top = -1;
+           
+           //case 1: when q1 is not empty
+           if (!q1.isEmpty()) {
+               while (!q1.isEmpty()) {
+                   top = q1.remove();
+                     q2.add(top);
+               }
+           } else { // case 2: when q2 is not empty
+               while (!q2.isEmpty()) {
+                   top = q2.remove();
+                   q1.add(top);
+               }
+           }
+              return top;
+        }
     }
-}
-}
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+        //output should be 5 4 3 2 1
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}                                              
