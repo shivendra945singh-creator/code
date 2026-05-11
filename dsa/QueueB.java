f466fce44acb6fe4cb26cc2ffd8931a82a917ed2
Shivendra Singh <shivendra945singh@gamil.com>
2026-05-11 21:47:26 +0530

    learn

--- dsa/QueueB.java
+++ dsa/QueueB.java
@@ -787,3 +787,39 @@
 
 
                                                 //implementing queue using deque
+
+
+ import java.util.*;
+ import java.util.LinkedList;
+ 
+    public class QueueB {
+        static class Queue {
+            Deque<Integer> dq = new LinkedList<>();
+    
+            public void add(int data) {
+                dq.addLast(data);
+            }
+    
+            public int remove() {
+                return dq.removeFirst();
+            }
+    
+            public int peek() {
+                return dq.getFirst();
+            }
+        }
+    
+        public static void main(String[] args) {
+            Queue q = new Queue();
+            q.add(1);
+            q.add(2);
+            q.add(3);
+            q.add(4);
+            q.add(5);
+    
+            System.out.println(q.peek()); // 1
+            System.out.println(q.remove()); // 1
+            System.out.println(q.remove()); // 2
+            System.out.println(q.peek()); // 3
+        }
+    }
