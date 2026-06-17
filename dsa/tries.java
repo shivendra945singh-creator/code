ad8adb0a264299bd2908f4b74e6280c0677ebd7e
Shivendra Singh <shivendra945singh@gamil.com>
2026-06-17 15:17:04 +0530

    l

--- dsa/tries.java
+++ dsa/tries.java
@@ -1,68 +1,68 @@
 
-                                                 //createa a trie data structure
+                                                 createa a trie data structure
 
-// import java.util.*;
+import java.util.*;
 
-// public class tries {
-//     static class Node{
-//         Node children[] = new Node[26];
-//         boolean eow = false;
+public class tries {
+    static class Node{
+        Node children[] = new Node[26];
+        boolean eow = false;
 
-//          Node(){
-//             for(int i=0;i<26;i++){
-//                 children[i] = null;
-//             }
-//         }
-//     }
+         Node(){
+            for(int i=0;i<26;i++){
+                children[i] = null;
+            }
+        }
+    }
 
-// public static Node root = new Node();//root node by deault  always empty
+public static Node root = new Node();//root node by deault  always empty
 
-// public static void main(String[] args) {
-//         String words[] = {"the","a","there","their","any"};
+public static void main(String[] args) {
+        String words[] = {"the","a","there","their","any"};
 
-//     }
-// }
+    }
+}
 
-                                                              //insert function
-                                                              
-
-// import java.util.*;
-// public class tries {
-//     static class Node{
-//         Node children[] = new Node[26];
-//         boolean eow = false;
-
-//          Node(){
-//             for(int i=0;i<26;i++){
-//                 children[i] = null;
-//             }
-//         }
-//     }
-
-//     public static Node root = new Node();//root node by deault  always empty
-
-//     public static void insert(String word){//O(L) L=length of word
-//         Node curr = root;
-//         for(int level=0;level<word.length();level++){
-//             int idx = word.charAt(level) - 'a';
-//             if(curr.children[idx] == null){
-//                 curr.children[idx] = new Node();
-//             }
-//             curr = curr.children[idx];
-//         }
-//         curr.eow = true;
-//     }
-
-//     public static void main(String[] args) {
-//         String words[] = {"the","a","there","their","any"};
-//         for(int i=0;i<words.length;i++){
-//             insert(words[i]);
-//         }
-//     }
-// }
+                                                              insert function
 
 
-                                                            //search function
+import java.util.*;
+public class tries {
+    static class Node{
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+         Node(){
+            for(int i=0;i<26;i++){
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();//root node by deault  always empty
+
+    public static void insert(String word){//O(L) L=length of word
+        Node curr = root;
+        for(int level=0;level<word.length();level++){
+            int idx = word.charAt(level) - 'a';
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static void main(String[] args) {
+        String words[] = {"the","a","there","their","any"};
+        for(int i=0;i<words.length;i++){
+            insert(words[i]);
+        }
+    }
+}
+
+
+                                                            search function
 
 
 
