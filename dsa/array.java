fecbde1159f82b10e611c5d317ccb3ea4da2aaba
Shivendra Singh <shivendra945singh@gamil.com>
2026-07-03 17:12:15 +0530

    l

--- dsa/array.java
+++ dsa/array.java
@@ -493,35 +493,35 @@
 
 
 
-public class array {
-    public static void maxsubarraysum( int numbers[]) {
-        int currsum = 0;
-        int maxsum = Integer.MIN_VALUE;
+// public class array {
+//     public static void maxsubarraysum( int numbers[]) {
+//         int currsum = 0;
+//         int maxsum = Integer.MIN_VALUE;
 
       
-        for (int i =0; i<numbers.length; i++) {
-            int start = i;
-            for(int j=i; j<numbers.length; j++) {
-                int end = j;
-                currsum = 0;
-                for ( int k = start; k<=end; k++) {
-                    currsum += numbers[k];  // subarray sum
-                }
-                System.err.println(currsum);
-                if (maxsum < currsum) {
-                    maxsum = currsum;
-                }
-            }
+//         for (int i =0; i<numbers.length; i++) {
+//             int start = i;
+//             for(int j=i; j<numbers.length; j++) {
+//                 int end = j;
+//                 currsum = 0;
+//                 for ( int k = start; k<=end; k++) {
+//                     currsum += numbers[k];  // subarray sum
+//                 }
+//                 System.err.println(currsum);
+//                 if (maxsum < currsum) {
+//                     maxsum = currsum;
+//                 }
+//             }
             
-        }
-        System.err.println("max sum = " + maxsum);
-    }
+//         }
+//         System.err.println("max sum = " + maxsum);
+//     }
 
-    public static void main(String[] args) {
-        int numbers[] = {1, -2, 6, -1, 3};
-        maxsubarraysum(numbers);
-    }
-    }
+//     public static void main(String[] args) {
+//         int numbers[] = {1, -2, 6, -1, 3};
+//         maxsubarraysum(numbers);
+//     }
+//     }
 
 
 
@@ -573,6 +573,7 @@ public class array {
 // -1
 // 2
 // 3
+
 
 
 // Maximum sum
